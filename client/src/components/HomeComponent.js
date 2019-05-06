c143b9fc31d33417086d3ffa0969ef4e2913ec6e
Michael B <michaelbarela17@gmail.com>
2019-05-06 16:13:12 -0700

    fixed syntax errors/homeComponent

--- client/src/components/HomeComponent.js
+++ client/src/components/HomeComponent.js
@@ -3,35 +3,35 @@ import React from 'react';
 function Home(props) {
     return(
         <div className="row row-content justify-content-center lead">
-            <div class="col-lg-3 text-center centered-links">
+            <div className="col-lg-3 text-center centered-links">
                 <span className="icons">
                     <i class="fa fa-home"aria-hidden="true"></i>
                 </span>
-                <h1 class="display-4">Buy</h1>
+                <h1 className="display-4">Buy</h1>
                 <p>Click Here For A List Of Homes To Buy</p>
-                <a href="#" class="btn btn-lg button-content" role="button">Purchase</a>
+                <a href="#" className="btn btn-lg button-content" role="button">Purchase</a>
             </div>
 
-            <hr class="clearfix w-100 d-lg-none" />
+            <hr className="clearfix w-100 d-lg-none" />
 
-            <div class="col-lg-3 text-center centered-links">
+            <div className="col-lg-3 text-center centered-links">
                 <span className="icons">
-                    <i class="fa fa-money"aria-hidden="true"></i>        
+                    <i className="fa fa-money"aria-hidden="true"></i>        
                 </span>
-                <h1 class="display-4">Rent</h1>
+                <h1 className="display-4">Rent</h1>
                 <p>Click Here For A List Of Homes To Rent</p>
-                <a href="#" class="btn btn-lg button-content" role="button">Rent</a>    
+                <a href="#" className="btn btn-lg button-content" role="button">Rent</a>    
             </div>
 
-            <hr class="clearfix w-100 d-lg-none" />
+            <hr className="clearfix w-100 d-lg-none" />
 
-            <div class="col-lg-3 text-center centered-links">
+            <div className="col-lg-3 text-center centered-links">
                 <span className="icons">
-                    <i class="fa fa-search-plus"aria-hidden="true"></i>            
+                    <i className="fa fa-search-plus"aria-hidden="true"></i>            
                 </span>
-                <h1 class="display-4">Find Agent</h1>
+                <h1 className="display-4">Find Agent</h1>
                 <p>Click Here To Find an Agent</p>
-                <a href="#" class="btn btn-lg button-content" role="button">Agent</a>        
+                <a href="#" className="btn btn-lg button-content" role="button">Agent</a>        
             </div>
       </div>
     );
