a3abf70b9cc5d6e3c8e1f7d82397307ab5728f4c
Michael B <michaelbarela17@gmail.com>
2019-05-06 14:52:30 -0700

    Added components

--- client/src/components/customers.js
+++ client/src/components/customers.js
@@ -1,32 +1,32 @@
-import React, { Component } from 'react';
-import './customer';
+// import React, { Component } from 'react';
+// import './customers';
 
-class Customers extends Component {
-  constructor() {
-    super();
-    this.state = {
-      customers: []
-    };
-  }
+// class Customers extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       customers: []
+//     };
+//   }
 
-  componentDidMount() {
-    fetch('/api/customers')
-      .then(res => res.json())
-      .then(customers => this.setState({customers}, () => console.log('Customers fetched...', customers)));
-  }
+//   componentDidMount() {
+//     fetch('/api/customers')
+//       .then(res => res.json())
+//       .then(customers => this.setState({customers}, () => console.log('Customers fetched...', customers)));
+//   }
 
-  render() {
-    return (
-      <div>
-        <h2>Customers</h2>
-        <ul>
-        {this.state.customers.map(customer => 
-          <li key={customer.id}>{customer.firstName} {customer.lastName}</li>
-        )}
-        </ul>
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div>
+//         <h2>Customers</h2>
+//         <ul>
+//         {this.state.customers.map(customer => 
+//           <li key={customer.id}>{customer.firstName} {customer.lastName}</li>
+//         )}
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
 
-export default Customers;
+// export default Customers;
