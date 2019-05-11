0d2a3722d8f69dc3d463a62042c3aea05a088d71
Michael B <michaelbarela17@gmail.com>
2019-05-11 13:35:59 -0700

    Added banner/404 page/ some routes

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,13 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import Banner from '../components/Banner'
+import {Link} from 'react-router-dom'
 export default function Home() {
-  return <Hero hero="defaultHero"></Hero>;
+  return <Hero>
+    <Banner title=" Luxurious Homes" subtitle="Our mission is to help you find your dream home">
+    <Link to='/houses' className="btn-primary">
+    Our Houses
+    </Link>
+    </Banner>
+  </Hero>;
 }
--- client/src/pages/Houses.js
+++ client/src/pages/Houses.js
@@ -1,11 +1,15 @@
 import React from 'react'
-
+import Hero from '../components/Hero'
+import Banner from '../components/Banner'
+import {Link} from 'react-router-dom'
 const Houses = () => {
-  return (
-    <div>
-      Hello from Houses page
-    </div>
-  )
-}
+  return <Hero hero="roomsHero">
+  <Banner title="Our Homes">
+  <Link to='/' className="btn-primary">
+  Return to Homepage
+  </Link>
+  </Banner>
+  </Hero>;
+};
 
 export default Houses
