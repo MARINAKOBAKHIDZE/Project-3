dcf3acd7d9aff876117d427579320a8cac7e2fc8
Michael B <michaelbarela17@gmail.com>
2019-05-11 10:29:46 -0700

    deleted components/added redirect

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,8 @@
 import React, { Component } from 'react';
-import './App.css';
-import Main from './components/MainComponent';
-import { BrowserRouter } from 'react-router-dom';
+//import './App.css';
 
-class App extends Component {
-
-    render() {
-        return (
-            <BrowserRouter>
-                <div className="App">
-                    <Main />
-                </div>
-            </BrowserRouter>
-        );
-    }
+function App() {
+    return <>hello from app</>
 }
 
 export default App;
