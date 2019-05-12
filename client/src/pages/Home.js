22d07fc21fbd9076b661e29f46aebdc5eae73056
Michael B <michaelbarela17@gmail.com>
2019-05-12 10:45:03 -0700

    added banner components

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,12 +2,18 @@ import React from 'react'
 import Hero from '../components/Hero'
 import Banner from '../components/Banner'
 import {Link} from 'react-router-dom'
+import Services from '../components/Services'
 export default function Home() {
-  return <Hero>
+  return (
+  <>
+  <Hero>
     <Banner title=" Luxurious Homes" subtitle="Our mission is to help you find your dream home">
     <Link to='/houses' className="btn-primary">
     Our Houses
     </Link>
     </Banner>
   </Hero>;
+  <Services/>
+  </>
+  );
 }
