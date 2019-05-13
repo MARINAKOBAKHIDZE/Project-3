6e490c65e989cb52ae5cd2468d7bb6ccece9e0d1
Michael B <michaelbarela17@gmail.com>
2019-05-12 18:32:01 -0700

    Syntax changes

--- client/src/pages/Houses.js
+++ client/src/pages/Houses.js
@@ -1,15 +1,21 @@
-import React from 'react'
-import Hero from '../components/Hero'
-import Banner from '../components/Banner'
-import {Link} from 'react-router-dom'
-const Houses = () => {
-  return <Hero hero="roomsHero">
-  <Banner title="Our Homes">
-  <Link to='/' className="btn-primary">
-  Return to Homepage
-  </Link>
-  </Banner>
-  </Hero>;
+import React from "react";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
+import RoomsContainer from "../components/RoomsContainer";
+const Rooms = () => {
+  return (
+    <>
+      <Hero hero="roomsHero">
+        <Banner title="our rooms">
+          <Link to="/" className="btn-primary">
+            return home
+          </Link>
+        </Banner>
+      </Hero>
+      <RoomsContainer />
+    </>
+  );
 };
 
-export default Houses
+export default Rooms;
